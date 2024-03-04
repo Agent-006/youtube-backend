6c04652e9fbfa9d9a9fbdace2a8e17dca729e1c6
Sagar~ <sagargope2001@gmail.com>
2024-03-04 16:07:37 +0530

    add tweet controllers and routes

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,24 +1,113 @@
-import mongoose, { isValidObjectId } from "mongoose";
-import { Tweet } from "../models/tweet.model.js";
-import { User } from "../models/user.model.js";
+import { Tweet } from "../models/tweet.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+
+    const { content } = req.body;
+
+    if (content.trim() === "") {
+        throw new ApiError(400, "Content is required");
+    }
+
+    const tweet = await Tweet.create({
+        owner: req.user?._id,
+        content,
+    });
+
+    if (!tweet) {
+        throw new ApiError(
+            500,
+            "Something went wrong while creating the tweet"
+        );
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, tweet, "Tweet created successfully"));
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+
+    const { userId } = req.params;
+
+    if (!userId) {
+        throw new ApiError(401, "User id is missing");
+    }
+
+    const allTweets = await Tweet.find({
+        owner: userId,
+    });
+
+    if (!allTweets) {
+        throw new ApiError(
+            500,
+            "Something went wrong while fetching all tweets"
+        );
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, allTweets, "Successfully fetched all tweets")
+        );
 });
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+
+    const { tweetId } = req.params;
+    const { content } = req.body;
+
+    if (!tweetId) {
+        throw new ApiError(401, "Tweet id is missing");
+    }
+
+    const updatedTweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            $set: {
+                content,
+            },
+        },
+        {
+            new: true,
+        }
+    );
+
+    if (!updatedTweet) {
+        throw new ApiError(500, "Something wrong while updating the tweet");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, updatedTweet, "Tweet updated successfully"));
 });
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+
+    const { tweetId } = req.params;
+
+    if (!tweetId) {
+        throw new ApiError(401, "Tweet id is missing");
+    }
+
+    const deletedTweet = await Tweet.findByIdAndDelete(tweetId);
+
+    if (!deletedTweet) {
+        throw new ApiError(
+            500,
+            "Something went wrong while deleting the tweet"
+        );
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Successfully deleted the tweet"));
 });
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet };
