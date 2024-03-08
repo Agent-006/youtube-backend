f230fd992582a17d79dec564dff3e0ccaa9e600d
Sagar~ <sagargope2001@gmail.com>
2024-03-08 14:58:40 +0530

    add dashboard controller and routes

--- src/app.js
+++ src/app.js
@@ -37,6 +37,7 @@ import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
@@ -47,5 +48,6 @@ app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter)
 
 export { app };
--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,17 +1,101 @@
 import mongoose from "mongoose";
-import { Video } from "../models/video.model.js";
-import { Subscription } from "../models/subscription.model.js";
-import { Like } from "../models/like.model.js";
+import { Video } from "../models/video.models.js";
+import { Subscription } from "../models/subscription.models.js";
+import { Like } from "../models/like.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    // TODO: Get the channel stats like 1. total video views, 2. total subscribers, 3. total videos, 4. total likes etc.
+    // we need subscriber collections - count the channel(user._id) in their subscribers -> subscription model
+
+    const subsCount = await Subscription.aggregate([
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(req.user?._id),
+            },
+        },
+        {
+            $count: "subscriberCount",
+        },
+    ]);
+
+    console.log(subsCount[0]);
+
+    // we need videos collections -> check video owner for total videos
+
+    const videosCount = await Video.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(req.user?._id),
+            },
+        },
+        {
+            $count: "videosCount",
+        },
+    ]);
+
+    console.log(videosCount[0]);
+
+    // we need videos collections -> queries the video views field
+
+    const totalViews = await Video.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(req.user?._id),
+            },
+        },
+        {
+            $group: {
+                _id: null,
+                totalViews: { $sum: "$views" },
+            },
+        },
+    ]);
+
+    console.log(totalViews[0]);
+
+    const allStats = {
+        totalSubscribers: subsCount[0]?.subscriberCount,
+        totalViews: totalViews[0]?.totalViews,
+        totalVideos: videosCount[0]?.videosCount,
+    };
+
+    console.log(allStats);
+
+    // we need likes collections -> query the likes -> check videos to get like count
+    
+
+    // TODO: res -> total views, total subs, total videos, total likes
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, allStats, "Stats fetched successfully"));
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const ChannelVideos = await Video.find({
+        owner: new mongoose.Types.ObjectId(req.user?._id),
+    });
+
+    if (!ChannelVideos) {
+        throw new ApiError(
+            500,
+            "Something went wrong while getting all channel videos"
+        );
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                ChannelVideos,
+                "Successfully fetched all channel videos"
+            )
+        );
 });
 
 export { getChannelStats, getChannelVideos };
